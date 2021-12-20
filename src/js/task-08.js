016fe58d6c749bc38afe4769c3182e56651f787d
Roman Zeno <zeno385@gmail.com>
2021-12-20 21:34:39 +0200

    01-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  const elem = e.currentTarget.elements;
+  const emailValue = elem.email.value;
+  const emailName = elem.email.name;
+  const passwordValue = elem.password.value;
+  const passwordName = elem.password.name;
+
+  if (emailValue === "" || passwordValue === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const obj = { [emailName]: emailValue, [passwordName]: passwordValue };
+
+  console.log(obj);
+
+  e.currentTarget.reset();
+}
