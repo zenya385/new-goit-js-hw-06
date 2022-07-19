8fac8e55b716b90fa050ec9f58f39d16ffdaa6a7
Roman Zeno <zeno385@gmail.com>
2022-07-19 12:50:35 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,10 @@
-
-
-const list = document.querySelector('#categories');
+const list = document.querySelector("#categories");
 const allItem = list.querySelectorAll(".item");
-console.log('~ allItem', allItem)
-// console.log(`Number of categories: ${allItem.length} `);
+console.log(`Number of categories: ${allItem.length} `);
 
-allItem.forEach((el)=>{
-  const lengthTitle = el.querySelector('h2').textContent;
+allItem.forEach((el) => {
+  const lengthTitle = el.querySelector("h2").textContent;
   console.log(`Category: ${lengthTitle}`);
   const array2 = el.querySelectorAll("li");
   console.log(`Elements ${array2.length}`);
-})
+});
